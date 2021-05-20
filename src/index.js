ceaf701c0cd8c1c6580876ede96acfa67fdab695
tavo <gasb_150@hotmail.com>
2021-05-19 20:02:54 -0500

    Add necesary files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TodoApp from './TodoApp'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>
+)
